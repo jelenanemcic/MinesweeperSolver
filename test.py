f4fe50cab50f41d1441efa9a88c3ca8fc736fcd9
Jelena Nemčić <nemcic.jelena9@gmail.com>
2020-12-13 21:13:50 +0100

    sitno nesto

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from cassowary import SimplexSolver, Variable, STRONG
 from random import sample, randint, choice
 
+
 class Field:
     # a[row][col]
     def __init__(self, i, j, is_mine=False):
@@ -15,36 +16,35 @@ class Field:
 
 
 class Board:
-    def __init__(self, n, k=0):
+    def __init__(self, n, k):
         """
         Create board object with dimensions nxn and k mines inside
         set at random locations
         """
         self.solver = SimplexSolver()
         self.board_dim = n
-        #self.num_mines = k
+        self.num_mines = k
         self.marked = []
         self.vars = [[Field(j, i) for i in range(n)] for j in range(n)]
 
-        #for (x, y) in zip(sample(range(n), k), sample(range(n), k)):
+        # for (x, y) in zip(sample(range(n), k), sample(range(n), k)):
         #    self.vars[x][y].is_mine = True
 
         self.current_adjacent_fields = []
 
-    #TODO: delete later, just for testing
+    # TODO: delete later, just for testing
     def set_mines(self, mines):
         self.num_mines = len(mines)
         for (row_index, col_index) in mines:
             self.vars[row_index][col_index].is_mine = True
-
 
     def _get_adjacent_fields(self, row_index, col_index):
         """
         Get a list of adjacent fields around field specified with row_index and col_index
         """
         adjacent_fields = []
-        for i in range(max(0, row_index-1), min(self.board_dim, row_index+2)):
-            for j in range(max(0, col_index-1), min(self.board_dim, col_index+2)):
+        for i in range(max(0, row_index - 1), min(self.board_dim, row_index + 2)):
+            for j in range(max(0, col_index - 1), min(self.board_dim, col_index + 2)):
                 if i != row_index or j != col_index:
                     adjacent_fields.append(self.vars[i][j])
         return adjacent_fields
@@ -73,7 +73,7 @@ class Board:
         field.covered = False
         print("otvaram {}: {}".format(field.variable, field.adjacent_mines))
         if field.is_mine:
-            #TOOD: not a ValueError, raise Explosion or something
+            # TODO: not a ValueError, raise Explosion or something
             raise ValueError("Boom")
         if field.adjacent_mines == 0:
             boundary_list = []
@@ -84,7 +84,6 @@ class Board:
         else:
             return [field]
 
-
     def make_constraint(self, row_index, col_index):
         """
         Compute equations for field (row_index, col_index)
@@ -93,19 +92,20 @@ class Board:
         adjacent_fields = self._get_adjacent_fields(row_index, col_index)
         adjacent_mines = self._get_adjacent_mines(row_index, col_index)
 
-        for field in adjacent_fields:
-            if field not in self.current_adjacent_fields:
-                self.current_adjacent_fields.append(field)
+        if adjacent_mines != 0:
+            for field in adjacent_fields:
+                if field not in self.current_adjacent_fields:
+                    self.current_adjacent_fields.append(field)
 
-        constraint = 0
-        for field in adjacent_fields:
-            if field.covered:
-                constraint += field.variable
-    #   print(constraint == adjacent_mines)
-        return constraint == adjacent_mines
+            constraint = 0
+            for field in adjacent_fields:
+                if field.covered:
+                    constraint += field.variable
+            print(constraint == adjacent_mines)
+            return constraint == adjacent_mines
 
     def get_random_field(self):
-        #TODO: we should somehow sample list of safe fields
+        # TODO: we should somehow sample list of safe fields
         while True:
             i = randint(0, self.board_dim - 1)
             j = randint(0, self.board_dim - 1)
@@ -120,26 +120,27 @@ class Board:
             for (col_index, var) in enumerate(row):
                 self.solver.add_constraint(self.vars[row_index][col_index].variable >= 0)
                 self.solver.add_constraint(self.vars[row_index][col_index].variable <= 1)
+
         # prefer corner here?
         newly_opened = self.get_random_field()
         self.open_field(newly_opened)
         visited = []
+        num_of_clear_fields = self.board_dim * self.board_dim - self.num_mines
 
-        # second condition?
-        while self.num_mines != len(self.marked) or len(visited) < 3:
+        while self.num_mines != len(self.marked) or len(visited) != num_of_clear_fields:
             print(newly_opened.variable)
 
-            self.solver.add_constraint(self.make_constraint(newly_opened.row, newly_opened.column))
+            constraint = self.make_constraint(newly_opened.row, newly_opened.column)
+            if constraint is not None:
+                self.solver.add_constraint(constraint)
             if newly_opened not in visited:
                 visited.append(newly_opened)
 
             possible_fields = []
-            found_mine = False
             for field in self.current_adjacent_fields:
                 if field.variable.value == 1:
                     print("oznacavam {}", field.variable)
                     field.marked_mine = True
-                    found_mine = True
                     if field not in self.marked:
                         self.marked.append(field)
                 if field.variable.value == 0 and field in self.marked:
@@ -152,7 +153,10 @@ class Board:
             else:
                 newly_opened = self.get_random_field()
 
-            self.open_field(newly_opened)
+            opened = self.open_field(newly_opened)
+            visited += opened
+            # kako bi uzeli nekog koji nije okružen nulama, napraviti pametnije?
+            newly_opened = visited[-1]
 
             print([[var.variable.value for var in row] for row in self.vars])
 
@@ -165,27 +169,27 @@ def test1():
     # X 0 X
     # 0 X 0
 
- #   b.vars[0][1].is_mine = True
+    #   b.vars[0][1].is_mine = True
     b.vars[1][0].is_mine = True
     b.vars[1][2].is_mine = True
     b.vars[2][1].is_mine = True
 
-  #  b.update()
+    #  b.update()
     # print(b.vars)
 
     # Vrijednosti trebeaju biti
     # 2 X 2
     # X 4 X
     # 2 X 2
-#    assert b.vars[0][0].adjacent_mines == 2
-#    assert b.vars[0][2].adjacent_mines == 2
-#    assert b.vars[1][1].adjacent_mines == 4
-#    assert b.vars[2][0].adjacent_mines == 2
-#    assert b.vars[2][2].adjacent_mines == 2
+    #    assert b.vars[0][0].adjacent_mines == 2
+    #    assert b.vars[0][2].adjacent_mines == 2
+    #    assert b.vars[1][1].adjacent_mines == 4
+    #    assert b.vars[2][0].adjacent_mines == 2
+    #    assert b.vars[2][2].adjacent_mines == 2
 
     # sva polja su zatvorena osim:
-   # b.vars[0][0].covered = False
-   # b.vars[0][2].covered = False
+    # b.vars[0][0].covered = False
+    # b.vars[0][2].covered = False
 
     b.solve()
 
@@ -209,12 +213,15 @@ def main1():
 
     assert x[0].value == 1 and x[1].value == 0 and x[2].value == 1
 
+
 def test2():
-    b = Board(4)
+    b = Board(4, 2)
     b.set_mines([(0, 1), (1, 2)])
     b.update()
 
     [print(f.variable, f.adjacent_mines) for f in b.open_field(b.vars[3][0])]
+    b.solve()
+    print([[var.variable.value for var in row] for row in b.vars])
 
 
 if __name__ == "__main__":
